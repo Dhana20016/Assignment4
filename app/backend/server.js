fb719c3939c1c80dfba52959b3aa3518deb9a267
Dhana20016 <poornidhananjani283@gmail.com>
2025-06-14 22:06:24 +0530

    Update server.js

--- app/backend/server.js
+++ app/backend/server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const helmet = require('helmet');
+app.use(helmet());
+
 app.use('/api/lessons', lessonRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
